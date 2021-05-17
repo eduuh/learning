bb362ff7da32d2c5c314d5608bbbe4f22c38e97a
eduuh <edwinkamaumuraya0@outlook.com>
2021-05-17 08:38:55 +0100

    Restructured

--- Networking/NetworkingAndStreams/fmmn/day3/log/ix-list.js
+++
@@ -1,9 +0,0 @@
-var level = require('level')
-var db = level('ix.db')
-var to = require('to2')
-
-db.createReadStream()
-  .pipe(to.obj(function (row, enc, next) {
-    console.log(row)
-    next()
-  }))
