02d911f30354f1b5cc5e7458d3412bb7653e7b90
eduuh <edwinkamaumuraya0@outlook.com>
2021-06-07 15:13:57 +0000

    feat: Finished working on the navbar

--- languages/css/React-Sass/reactsass/src/App.tsx
+++ languages/css/React-Sass/reactsass/src/App.tsx
@@ -1,24 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './sass/main.scss';
+import sprite from "./images/sprite.svg"
+import logo from "./images/logo.png"
+import user from "./images/user.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">
+        <img src={logo} alt="trillo log" className="logo" />
+
+        <form action="#" className="search">
+          <input type="text" className="search__input" placeholder="Search Hotels" />
+          <button className="search__button">
+            <svg className="search__icon">
+              <use xlinkHref={`${sprite}#icon-magnifying-glass`}></use>
+            </svg>
+          </button>
+        </form>
+
+        <nav className="user-nav">
+          <div className="user-nav__icon-box">
+            <svg className="user-nav__icon">
+              <use xlinkHref={`${sprite}#icon-bookmark`} />
+            </svg>
+            <span className="user-nav__notification">6</span>
+          </div>
+
+          <div className="user-nav__icon-box">
+            <svg className="user-nav__icon">
+              <use xlinkHref={`${sprite}#icon-chat`} />
+            </svg>
+            <span className="user-nav__notification">6</span>
+          </div>
+
+          <div className="user-nav__user">
+            <img src={user} alt="User" className="user-nav__user-photo" />
+            <span className="user-nav__user-name">Edwin</span>
+          </div>
+        </nav>
+
+      </div>
+
+      <div className="content">
+        <nav className="sidebar">
+          navigation
+          </nav>
+
+        <main className="hotel-view">
+          hotel view
+        </main>
+      </div>
     </div>
   );
 }
